12c53a690e759a5e5aac479a477004ff97a35859
Holly Allen <hja@alum.mit.edu>
2017-07-20 21:30:02 -0700

    works in more cases
    Parsing now works even if any given day has no schedule table. Testing
    revealed that since starting this work in March 2017, the schedule
    tables have changed and the column indexes had to change. Overall this
    script remains very brittle.

--- pool.py
+++ pool.py
@@ -5,8 +5,11 @@ from pprint import pprint
 just_the_facts = []
 
 schedule_div_id = "tab-3"
+no_classes = "There are no Classes at this time"
 
-req = Request('http://sfrecpark.org/destination/herz-playground/coffman-pool/', headers={'User-Agent': 'Mozilla/5.0'})
+#req = Request('http://sfrecpark.org/destination/herz-playground/coffman-pool/', headers={'User-Agent': 'Mozilla/5.0'})
+#req = Request('http://sfrecpark.org/destination/balboa-park/balboa-pool/', headers={'User-Agent': 'Mozilla/5.0'})
+req = Request('http://sfrecpark.org/destination/bay-view-playground/martin-luther-king-jr-pool/', headers={'User-Agent': 'Mozilla/5.0'})
 page = urlopen(req).read()
 soup = BeautifulSoup(page,"html.parser")
 
@@ -16,23 +19,22 @@ sched = soup.find(id=schedule_div_id)
 # all the days in order, not guardanteed to start on any particular day
 days = sched.find_all('h3')
 
-# all the daily schedule tables
-# this doesn't work if some days have no times, thus no table
-tables = sched.find_all('table')
+# look at each day
+for day in days:
+	day_name = day.string
+	for sibling in day.next_siblings:
+		# looking for either a table or p
+		if sibling.name == "table":
+			rows = sibling.find_all('tr')
+			j = 0
+			for row in rows:
+			    cols = row.find_all('td')
+			    cols = [ele.text.strip() for ele in cols]
+			    if j > 0: # skip the header row
+			    	just_the_facts.append([day_name,cols[5],cols[1],cols[2]])
+			    j=j+1
+		if sibling.string == no_classes:
+			break
 
-if (len(days) != len(tables)):
-	pprint("wrong length!")
-
-i = 0 # needed to pull day of the week
-for table in tables:
-	rows = table.find_all('tr')
-	j = 0
-	for row in rows:
-	    cols = row.find_all('td')
-	    cols = [ele.text.strip() for ele in cols]
-	    if j > 0: # skip the header row
-	    	just_the_facts.append([days[i].get_text(),cols[4],cols[2],cols[3],cols[7]])
-	    j=j+1
-	i=i+1
 
 pprint(just_the_facts)
