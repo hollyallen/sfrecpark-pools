be2b41c1dd650a1727bc6176ebb2b3338415e07e
Holly Allen <hja@alum.mit.edu>
2017-08-11 23:34:22 -0700

    A web page!
    A real life webpage with simple filtering and almost no styling yet.

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # sfrecpark-pools
 This is a tool for deciding when to swim at [San Francisco public pools](http://sfrecpark.org/recreation-community-services/aquatics-pools/).
 
-Right now there is a web scraper, pool.py, for gathering pool swim times from [sfrecpark.org](http://sfrecpark.org) webpages. 
-Next up is a nice display of the data and a useful way to filter for the times and kind of swimming you want to do.
+First run the web scraper, pool.py, to gather pool swim times from [sfrecpark.org](http://sfrecpark.org) webpages. 
+
+Then run a local web server (for example 'python -m SimpleHTTPServer 8000') to serve up the
+html and javascript. This page reads the csv file and provides some simple filtering.
 
 ## Getting Started
 
 ### Prerequisites
 
-You'll need [Python 3.x](https://www.python.org/downloads/) and [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/).
+To run the python script you'll need [Python 3.x](https://www.python.org/downloads/),
+[BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/), and 
+[Pandas](http://pandas.pydata.org/).
+
+To run the webpage you'll need a local copy of the [Papaparse](http://papaparse.com/)
+library (minified version) to parse the csv file that you python script created.
+
---
+++ script.js
@@ -0,0 +1,97 @@
+function loadData() {
+	var csvfile = "data.csv";
+	$.get(csvfile, function (data) {
+	        var csvdata = Papa.parse(data, {header:false});
+	        console.log(csvdata.data);
+			createTable(csvdata.data);
+	    });
+}
+
+function createTable(input) {
+	table = document.createElement("table");
+	table.id = "poolTable";
+	// create 10 table rows, each with two cells
+	for (var i=0; i < input.length; i++) {
+    	var row = table.insertRow();  // DOM method for creating table rows
+    	for (var j=0; j < 5; j++) {
+	    	var cell = row.insertCell(j);  	    	
+	    	cell.innerHTML = input[i][j];
+    	}
+  	}
+  	document.body.appendChild( table);
+}
+
+function searchPool() {
+	// Declare variables 
+	var input, filter, table, tr, td, i;
+	input = document.getElementById("inputPool");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("poolTable");
+	tr = table.getElementsByTagName("tr");
+
+	// Clear out other search input fields
+	document.getElementById("inputDay").value = "";
+	document.getElementById("inputClass").value = "";
+
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 0; i < tr.length; i++) {
+    	td = tr[i].getElementsByTagName("td")[0];
+    	if (td) {
+      		if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        		tr[i].style.display = "";
+		    } else {
+        		tr[i].style.display = "none";
+	    	}
+		} 
+	}
+}
+
+function searchDay() {
+	// Declare variables 
+	var input, filter, table, tr, td, i;
+	input = document.getElementById("inputDay");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("poolTable");
+	tr = table.getElementsByTagName("tr");
+
+	// Clear out other search input fields
+	document.getElementById("inputPool").value = "";
+	document.getElementById("inputClass").value = "";
+
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 0; i < tr.length; i++) {
+    	td = tr[i].getElementsByTagName("td")[1];
+    	if (td) {
+      		if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        		tr[i].style.display = "";
+		    } else {
+        		tr[i].style.display = "none";
+    		}
+	    } 
+	}
+}
+
+function searchClass() {
+	// Declare variables 
+	var input, filter, table, tr, td, i;
+	input = document.getElementById("inputClass");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("poolTable");
+	tr = table.getElementsByTagName("tr");
+
+	// Clear out other search input fields
+	document.getElementById("inputPool").value = "";
+	document.getElementById("inputDay").value = "";
+
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 0; i < tr.length; i++) {
+    	td = tr[i].getElementsByTagName("td")[2];
+    	if (td) {
+      		if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        		tr[i].style.display = "";
+		    } else {
+        		tr[i].style.display = "none";
+    		}
+	    } 
+	}
+}
